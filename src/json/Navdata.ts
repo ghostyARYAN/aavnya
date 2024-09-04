1ec128e1f66948ff191ed1d433d1858fec35ac27
awadhiyanaveen39 <awadhiyanaveen39@gmail.com>
2024-09-04 00:35:42 +0000

    update

---
+++ src/json/Navdata.ts
@@ -0,0 +1,34 @@
+export const Navdata = [
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "Sponsers",
+    link: "/sponsers",
+  },
+  {
+    name: "Coursers",
+    link: "/courses",
+  },
+  {
+    name: "Teams",
+    link: "/teams",
+  },
+  {
+    name: "Galary",
+    link: "/galary",
+  },
+  {
+    name: "Events",
+    link: "/events",
+  },
+  {
+    name: "About Us",
+    link: "/about",
+  },
+  {
+    name: "Contact US",
+    link: "/contact",
+  },
+];
